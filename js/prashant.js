45d65416a339b123de71bbe1b2e1cc7a83ab5b3d
Surya Prashanth <surya@hackerearth.com>
2016-06-22 12:59:06 +0530

    Added all files

---
+++ js/prashant.js
@@ -0,0 +1,11 @@
+function home(){
+	var id = document.getElementById('div_main').innerHTML="<h1>TECHNICAL ADVANCEMENTS IN MECHANICAL ENGINEERING</h1><br><br>
+                        <img src="img/gears.gif" />
+                        <h1>22nd &23rd , July 2016</h1>";
+
+}
+function conf(){
+	var id = document.getElementById('div_main').innerHTML="<h1>TECHNICAL ADVANCEMENTS IN MECHANICAL ENGINEERING</h1><br><br>
+                        <img src="img/gears.gif" />
+                        <h1>Prashant</h1>";	
+}
