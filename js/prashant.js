265d0537eb0d1ef7e69e4b0cbbc3628a99a6da63
Surya Prashanth <surya@hackerearth.com>
2016-06-23 00:13:00 +0530

    add highlight

--- js/prashant.js
+++ js/prashant.js
@@ -7,5 +7,13 @@ function home(){
 function conf(){
 	var id = document.getElementById('div_main').innerHTML="<h1>TECHNICAL ADVANCEMENTS IN MECHANICAL ENGINEERING</h1><br><br>
                         <img src="img/gears.gif" />
-                        <h1>Prashant</h1>";	
+                        <h1>Prashant</h1>";
 }
+
+$(document).ready(function(){
+    var $links = $('.link').click(function(e){
+        $links.removeClass('highlight');
+        $(this).addClass('highlight');
+    });
+});
+
